860cedbb448ba36f113a862bcbea09d161e93a12
Ghab082702 <ghabmayoralgo0827@gmail.com>
2025-07-23 02:02:14 +0800

    ChatClient added

--- src/com/ghab/javachatapp/client/ChatClient.java
+++ src/com/ghab/javachatapp/client/ChatClient.java
@@ -1,10 +1,41 @@
 package com.ghab.javachatapp.client;
+import java.io.*;
+import java.net.*;
 
 public class ChatClient {
+    private Socket socket = null;
+    private BufferedReader inputConsole = null;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
 
-    // Practice Git Commit
+    public ChatClient (String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected to chat server");
 
-    // Practice for pulling
+            inputConsole = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-    // Second Practice
+            String line = "";
+            while (!line.equals("exit")){
+                line = inputConsole.readLine();
+                out.println(line);
+                System.out.println(in.readLine());
+            }
+
+            socket.close();
+            inputConsole.close();
+            out.close();
+        } catch (UnknownHostException u) {
+            System.out.println("Host Unknown: " + u.getMessage());
+        } catch (IOException i) {
+            System.out.println("Unexpected exception: " + i.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatClient client = new ChatClient("127.0.0.1" , 5000);
+    }
+
 }
